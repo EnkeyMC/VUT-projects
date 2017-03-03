9e11c43d6e1d70da80fe99aafbd7e7776eb362f0
Martin Omacht <enkeymc@gmail.com>
2017-03-03 13:53:44 -0500

    [BBT] Fixture object + 2 tests

--- black_box_tests.cpp
+++ black_box_tests.cpp
@@ -3,12 +3,12 @@
 // Purpose:     Red-Black Tree - public interface tests
 //
 // $NoKeywords: $ivs_project_1 $black_box_tests.cpp
-// $Author:     JMENO PRIJMENI <xlogin00@stud.fit.vutbr.cz>
+// $Author:     MARTIN OMACHT <xomach00@stud.fit.vutbr.cz>
 // $Date:       $2017-01-04
 //============================================================================//
 /**
  * @file black_box_tests.cpp
- * @author JMENO PRIJMENI
+ * @author MARTIN OMACHT
  * 
  * @brief Implementace testu binarniho stromu.
  */
@@ -32,5 +32,46 @@
 //    - Vsechny cesty od kazdeho listoveho uzlu ke koreni stromu obsahuji
 //      *STEJNY* pocet cernych uzlu.
 //============================================================================//
+
+/// Fixture for testing class BinaryTree
+class BinaryTreeTest : public ::testing::Test {
+	protected:
+		BinaryTree* tree;
+		
+		BinaryTreeTest() {
+			this->tree = new BinaryTree();
+		}
+
+		~BinaryTreeTest() {
+			delete this->tree;
+		}
+};
+
+// PUBLIC INTERFACE TESTS
+
+TEST_F(BinaryTreeTest, InsertNodeToEmptyTree) {
+	auto retVal = this->tree->InsertNode(1);
+	ASSERT_TRUE(retVal.first);
+	ASSERT_NE(retVal.second, nullptr);
+}
+
+TEST_F(BinaryTreeTest, InsertDuplicateNode) {
+	auto firstNode = this->tree->InsertNode(1);
+	auto duplicate = this->tree->InsertNode(1);
+	
+	EXPECT_FALSE(duplicate.first);
+	EXPECT_EQ(duplicate.second, firstNode.second);
+}
+
+// AXIOM TESTS
+
+// MAIN
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
+
 
 /*** Konec souboru black_box_tests.cpp ***/
